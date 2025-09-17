51484b5f684bd52abcbaac826b86d3331dbf69f7
atikoo67 <atinafbedasaa@gmail.com>
2025-09-17 22:17:16 +0300

    final profile page

--- lib/features/movie/presentation/pages/profile.dart
+++ lib/features/movie/presentation/pages/profile.dart
@@ -16,12 +16,103 @@ class _MyProfileState extends State<MyProfile> {
     final profile = args['profile'] as Map<String, dynamic>;
     final theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(title: Text('Profile', style: theme.textTheme.bodyMedium)),
+      appBar: AppBar(
+        title: Text('Profile', style: theme.textTheme.bodyMedium),
+        centerTitle: true,
+      ),
       body: Column(
+        spacing: 20,
         children: [
-          Text("${profile['email']}"),
-          Text("${profile["username"]}"),
-          Text("${profile["phonenumber"]}"),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 10,
+              children: [
+                CircleAvatar(
+                  backgroundColor: theme.colorScheme.surface,
+                  radius: 30,
+                  child: Image.asset(
+                    "assets/launch_icon/my_logo.png",
+                    height: 40,
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${profile['username']}',
+                      style: theme.textTheme.bodySmall,
+                    ),
+                    Text('Id:13742857', style: theme.textTheme.displaySmall),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              spacing: 10,
+              children: [
+                Text('User email', style: theme.textTheme.bodySmall),
+                Container(
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.surface,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+
+                  child: Text(
+                    "${profile['email']}",
+                    style: theme.textTheme.headlineLarge,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              spacing: 10,
+              children: [
+                Text('Username', style: theme.textTheme.bodySmall),
+                Container(
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.surface,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+
+                  child: Text(
+                    "${profile["username"]}",
+                    style: theme.textTheme.headlineLarge,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              spacing: 10,
+              children: [
+                Text('Phone Number', style: theme.textTheme.bodySmall),
+                Container(
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.surface,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+
+                  child: Text(
+                    "${profile["phonenumber"]}",
+                    style: theme.textTheme.headlineLarge,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
