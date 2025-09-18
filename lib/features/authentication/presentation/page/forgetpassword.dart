15e90a251b18ad092aff6fc69f71d9f8da48e95f
atikoo67 <atinafbedasaa@gmail.com>
2025-09-18 23:58:20 +0300

    add a button, and changed the ui

--- lib/features/authentication/presentation/page/forgetpassword.dart
+++ lib/features/authentication/presentation/page/forgetpassword.dart
@@ -1,5 +1,4 @@
-import 'dart:ui';
-
+import 'package:quote/cores/components/buttons/textbutton.dart';
 import 'package:quote/cores/utils/theme/textstyle.dart';
 import 'package:quote/cores/components/buttons/mybutton.dart';
 import 'package:quote/cores/components/textfields/phonenumber_field.dart';
@@ -28,146 +27,71 @@ class _ForgetPasswordState extends State<ForgetPassword> {
       appBar: AppBar(
         centerTitle: true,
         elevation: 0,
-        foregroundColor: theme.textTheme.displayLarge!.color,
-        backgroundColor: theme.scaffoldBackgroundColor,
 
-        title: Text(
-          'Forgot Password',
-          style: TextStyle(
-            fontSize: ScreenSize.screenHeight(context) * 0.025,
-            fontWeight: theme.textTheme.displayLarge!.fontWeight,
-          ),
-        ),
+        title: Text("Reset Password", style: AppTextStyle.textTheme.bodyLarge),
       ),
       body: SafeArea(
         bottom: false,
         child: SingleChildScrollView(
           keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          child: Stack(
-            children: [
-              Positioned(
-                child: Stack(
-                  children: [
-                    Image.asset(
-                      'assets/background.jpg',
-                      fit: BoxFit.cover,
-                      height: ScreenSize.screenHeight(context) * 0.89,
-                      width: MediaQuery.of(context).size.width,
-                    ),
-                    BackdropFilter(
-                      filter: ImageFilter.blur(
-                        sigmaX: 2,
-                        sigmaY: 3,
-                        tileMode: TileMode.mirror,
-                      ),
-                      child: Container(
-                        height: ScreenSize.screenHeight(context) * 0.89,
-                        color: theme.colorScheme.surface,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: ScreenSize.screenHeight(context) * 0.89,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Center(
-                      child: Icon(
-                        Icons.person_pin_rounded,
-                        size: ScreenSize.screenHeight(context) * 0.0800,
-                        color: theme.iconTheme.color,
-                      ),
-                    ),
-
-                    SizedBox(
-                      height: ScreenSize.screenHeight(context) * 0.78,
-                      child: Container(
-                        padding: EdgeInsets.symmetric(vertical: 40),
-                        decoration: BoxDecoration(
-                          color: theme.colorScheme.surface,
-                          borderRadius: BorderRadius.vertical(
-                            top: Radius.circular(30),
+          child: SizedBox(
+            height: ScreenSize.screenHeight(context) * 0.8,
+            child: Column(
+              spacing: 10,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 20,
+                  ),
+                  child: Column(
+                    spacing: 25,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Forgot your password?',
+                            style: theme.textTheme.bodyLarge,
                           ),
-                        ),
-                        child: Column(
-                          spacing: ScreenSize.screenHeight(context) * 0.010,
+                          Text(
+                            "Enter your phone number below and we’ll send you a code to reset your password.",
+                            style: theme.textTheme.bodySmall,
+                          ),
+                        ],
+                      ),
+                      PhoneNumberField(
+                        phoneNumberController: phoneNumberController,
+                        onChanged: (phone) {
+                          setState(() {
+                            fullPhoneNumber = phone.completeNumber;
+                          });
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 25,
-                                vertical: 20,
-                              ),
-                              child: Row(
-                                children: [
-                                  Text(
-                                    'Forgot your password? Don\'t Worry!',
-                                    style: TextStyle(
-                                      fontSize:
-                                          ScreenSize.screenHeight(context) *
-                                          0.021,
-                                      fontWeight: FontWeight.w500,
-                                      color:
-                                          theme.textTheme.displayLarge!.color,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 20,
-                              ),
-                              child: Text(
-                                "Please provide your registered phone number. We’ll send you a verification code to reset your password.",
-                                style: TextStyle(
-                                  fontSize:
-                                      ScreenSize.screenHeight(context) * 0.016,
-                                  fontWeight: FontWeight.w500,
-                                  color: theme.textTheme.displayMedium!.color,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: ScreenSize.screenHeight(context) * 0.02,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.symmetric(
-                                horizontal:
-                                    ScreenSize.screenHeight(context) * 0.025,
-                              ),
-                              child: PhoneNumberField(
-                                phoneNumberController: phoneNumberController,
-                                onChanged: (phone) {
-                                  setState(() {
-                                    fullPhoneNumber = phone.completeNumber;
-                                    // print(fullPhoneNumber); // e.g. +251912345678
-                                  });
-                                },
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                MyButton(
-                                  onTap: resetPassword,
-                                  child: Text(
-                                    'Send',
-                                    style: AppTextStyle.textTheme.titleMedium,
-                                  ),
-                                ),
-                              ],
+                            MyTextButton(
+                              text: 'Resend Code',
+                              onPressed: () async {},
                             ),
                           ],
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+
+                MyButton(
+                  onTap: resetPassword,
+                  child: Text('Send Code', style: theme.textTheme.titleSmall),
+                ),
+              ],
+            ),
           ),
         ),
       ),
